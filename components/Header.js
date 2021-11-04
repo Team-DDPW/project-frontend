1aeba8afbb64b12238d891ede4c2807b3c7de9fc
Daniel Dills <dills253@gmail.com>
2021-11-03 20:09:52 -0700

    continued work with materialUI

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,17 @@ import AppBar from '@mui/material/AppBar';
 import CameraIcon from '@mui/icons-material/PhotoCamera';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { Button } from '@mui/material';
+import { withStyles } from '@material-ui/core/styles';
+// const useStyles = makeStyles({
+//   heading1: {
+//     flexGrow: 1,
+//   },
+// });
 
 function Header() {
+  const classes = useStyles();
+
   return (
     <AppBar position="relative">
       <Toolbar>
@@ -11,6 +20,7 @@ function Header() {
         <Typography variant="h6" color="inherit" noWrap>
           FastEx project
         </Typography>
+        <Button color="error">Sign Up</Button>
       </Toolbar>
     </AppBar>
   );
