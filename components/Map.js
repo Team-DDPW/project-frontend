70504811c2584558fb99eec156d0b6f0b697758d
Prabin Singh <pravin544@gmail.com>
2021-11-05 22:01:24 -0500

    added maps

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
+const containerStyle = {
+  width: '1000px',
+  height: '700px',
+};
+
+const center = {
+  lat: 31.9686,
+  lng: -99.9018,
+};
 
 function Map() {
-    return (
-        <div>
-          <h1>Map</h1>  
-        </div>
-    )
+  return (
+    <LoadScript googleMapsApiKey="AIzaSyC5BvRIbwnwfsZiON0Pg8W_KUrSLJ5cFoQ">
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={5}>
+        {/* Child components, such as markers, info windows, etc. */}
+        <Marker
+          position={{
+            lat: 31.9686,
+            lng: -99.9018,
+          }}
+        />
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  );
 }
 
-export default Map
+export default Map;
+
+
