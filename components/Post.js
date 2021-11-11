0045fa0fc78bd0315823fae9b268dc861e3d64fb
Davee Sok <daveesok@gmail.com>
2021-11-10 18:55:54 -0600

    cleaned up forms

--- components/Post.js
+++ components/Post.js
@@ -8,30 +8,10 @@ function Post() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const [itemName, setitemName] = useState('');
-  const [itemDescription, setitemDescription] = useState('');
-  const [weight, setWeight] = useState('');
-  const [oCountry, setOCountry] = useState('');
-  const [dCountry, setDCountry] = useState('');
-  const [deadline, setDeadline] = useState('');
-  const [priceOffer, setPriceOffer] = useState('');
-
-  // const [state, setState] = useState({ itemName: '', itemDescription: '' });
-
-  // const handleChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setState((prevState) => ({
-  //     ...prevState,
-  //     [name]: value,
-  //   }));
-  // };
-  const [fields, handleFieldChange] = useFormFields({
-    itemName: '',
-    itemDescription: '',
-  });
+  const [fields, handleFieldChange] = useFormFields({});
 
   const onSubmit = () => {
-    console.log(fields);
+    console.log('fields line 34', fields);
   };
 
   return (
@@ -47,42 +27,34 @@ function Post() {
         <Modal.Body>
           <Form>
             <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Group as={Col} controlId="item_name">
                 <Form.Label>Item Name</Form.Label>
-                <Form.Control
-                  autoFocus
-                  type="email"
-                  value={fields.email}
-                  onChange={handleFieldChange}
-                />
+                <Form.Control autoFocus onChange={handleFieldChange} />
               </Form.Group>
             </Row>
 
-            <Form.Group className="mb-3" controlId="formGridAddress1">
+            <Form.Group className="mb-3" controlId="item_description">
               <Form.Label>Item Description</Form.Label>
+              <Form.Control onChange={handleFieldChange} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="weight">
+              <Form.Label>Weight</Form.Label>
               <Form.Control
-                type="password"
-                value={fields.password}
                 onChange={handleFieldChange}
+                placeholder="Package Weight"
               />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formGridAddress2">
-              <Form.Label>Weight</Form.Label>
-              <Form.Control placeholder="Package Weight" />
-            </Form.Group>
-
             <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridCity">
+              <Form.Group as={Col} controlId="origin_country">
                 <Form.Label>Country of Origin</Form.Label>
-                <Form.Control />
+                <Form.Control onChange={handleFieldChange} />
               </Form.Group>
 
-              <Form.Group as={Col} controlId="formGridState">
-                <Form.Group as={Col} controlId="formGridCity">
-                  <Form.Label>Destination Country</Form.Label>
-                  <Form.Control />
-                </Form.Group>
+              <Form.Group as={Col} controlId="destination_country">
+                <Form.Label>Destination Country</Form.Label>
+                <Form.Control onChange={handleFieldChange} />
               </Form.Group>
             </Row>
 
@@ -94,7 +66,7 @@ function Post() {
             <Row className="mb-3">
               <Form.Group as={Col} controlId="formGridCity">
                 <Form.Label>Price Offer</Form.Label>
-                <Form.Control />
+                <Form.Control onChange={handleFieldChange} />
               </Form.Group>
             </Row>
 
@@ -104,12 +76,13 @@ function Post() {
             </Form.Group>
           </Form>
         </Modal.Body>
+
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Cancel
           </Button>
           <Button variant="primary" className="btnFormSend" onClick={onSubmit}>
-            Save Changes
+            Submit
           </Button>
         </Modal.Footer>
       </Modal>
